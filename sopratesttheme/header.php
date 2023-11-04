e532e61c1260ae6ba94c0220ee3dc7e43dfaa086
joseD <josed3000@gmail.com>
2023-11-04 16:57:24 +0100

    last call header

--- sopratesttheme/header.php
+++ sopratesttheme/header.php
@@ -15,8 +15,8 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Featuressl</a>
-                <a class="nav-link" href="#">Pricingg</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
                 <a class="nav-link disabled">Disabled</a>
             </div>
             </div>
